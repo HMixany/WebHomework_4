accd983e4f482d0efd4fc3db3b569c0c0a429540
HMixany <hmixany@gmail.com>
2023-11-15 22:42:11 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
+import mimetypes
+import urllib.parse
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from pathlib import Path
+
+
+BASE_DIR = Path()
 
 
 class MyFramework(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        with open('index.html', 'rb') as file:
-            self.wfile.write(file.read())
+        print(urllib.parse.urlparse(self.path))
+        route = urllib.parse.urlparse(self.path)
+        match route.path:
+            case "/":
+                self.send_html('index.html')
+            case "/message.html":
+                self.send_html('message.html')
+            case _:
+                file = BASE_DIR.joinpath(route.path[1:])
+                if file.exists():
+                    self.send_static(file)
+                else:
+                    self.send_html('error.html', status_code=404)
 
     def do_POST(self):
         pass
+
+    def send_html(self, filename, status_code=200):
+        self.send_response(status_code)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as file:
+            self.wfile.write(file.read())
+
+    def send_static(self, filename, status_code=200):
+        self.send_response(status_code)
+        mime_type, *_ = mimetypes.guess_type(filename)
+        if mime_type:
+            self.send_header('Content-Type', mime_type)
+        else:
+            self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        with open(filename, 'rb') as file:
+            self.wfile.write(file.read())
 
 
 def run_server():
